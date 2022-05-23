d622e39ee42e773d80e8e645b40ab9dabf78578f
julesblaise <giulio201290@live.it>
2022-05-23 14:23:58 +0100

    rinkeby test

--- backend/src/config.js
+++ backend/src/config.js
@@ -7,24 +7,87 @@ const { NETWORK } = require(`${basePath}/constants/network.js`);
 const network = NETWORK.eth;
 
 // General metadata for Ethereum
-const namePrefix = "YOUR COLLECTION NAME";
-const description = "Remember to replace this description";
+const namePrefix = "True Italian Pizza Army";
+const description = "The True Italian Pizza Army, aka TIPA, traveling around different worlds and galaxies looking for other flavors of Pizza Soldiers. Will they find peace, or are they just walking into battle?";
 const baseUri = "ipfs://NewUriToReplace"; // This will be replaced automatically
 
 // If you have selected Solana then the collection starts from 0 automatically
 const layerConfigurations = [
   {
-    growEditionSizeTo: 5,
+    growEditionSizeTo: 843,
     layersOrder: [
       { name: "Background" },
-      { name: "Eyeball" },
-      { name: "Eye color" },
-      { name: "Iris" },
-      { name: "Shine" },
-      { name: "Bottom lid" },
-      { name: "Top lid" },
+      { name: "Base" },
+      { name: "Rossa Topping" },
+      { name: "Eyes Red" },
+      { name: "Mouth" },
+      { name: "Red Nose" },
+      { name: "Hats" },
+      { name: "Weapons" },
     ],
   },
+  {
+    growEditionSizeTo: 1686,
+    layersOrder: [
+      { name: "Background" },
+      { name: "Base" },
+      { name: "Bianca Topping" },
+      { name: "Eyes Yellow" },
+      { name: "Mouth" },
+      { name: "Yellow Nose" },
+      { name: "Hats" },
+      { name: "Weapons" },
+    ],
+  },
+  {
+    growEditionSizeTo: 1761,
+    layersOrder: [
+      { name: "Background" },
+      { name: "Base" },
+      { name: "Topping" },
+      { name: "Fake Goggles" },
+      { name: "Mouth" },
+      { name: "Fake Nose" },
+      { name: "Hats" },
+      { name: "Weapons" },
+    ],
+  },
+{
+  growEditionSizeTo: 1800,
+  layersOrder: [
+    { name: "Background" },
+    { name: "Base" },
+    { name: "Topping" },
+    { name: "Pulcinella Mask" },
+    { name: "Mouth 2" },
+    { name: "Hats" },
+    { name: "Weapons" },
+  ],
+},
+{
+  growEditionSizeTo: 1847,
+  layersOrder: [
+    { name: "Background" },
+    { name: "Base" },
+    { name: "Bianca Topping" },
+    { name: "Eyes Yellow" },
+    { name: "Samurai Mask Yellow" },
+    { name: "Hats" },
+    { name: "Weapons" },
+  ],
+},
+{
+  growEditionSizeTo: 1889,
+  layersOrder: [
+    { name: "Background" },
+    { name: "Base" },
+    { name: "Rossa Topping" },
+    { name: "Eyes Red" },
+    { name: "Samurai Mask Red" },
+    { name: "Hats" },
+    { name: "Weapons" },
+  ],
+},
 ];
 
 const shuffleLayerConfigurations = true;
@@ -32,13 +95,13 @@ const shuffleLayerConfigurations = true;
 const debugLogs = false;
 
 const format = {
-  width: 512,
-  height: 512,
+  width: 2500,
+  height: 2500,
   smoothing: false,
 };
 
 const extraMetadata = {
-  external_url: "https://codecats.xyz", // Replace with your website or remove this line if you do not have one.
+  external_url: "", // Replace with your website or remove this line if you do not have one.
 };
 
 // NFTPort Info
@@ -49,34 +112,34 @@ const LIMIT = 2; // Your API key rate limit
 const CHAIN = 'rinkeby'; // only rinkeby or polygon
 
 // REQUIRED CONTRACT DETAILS THAT CANNOT BE UPDATED LATER!
-const CONTRACT_NAME = 'CRYPTOPUNKS';
-const CONTRACT_SYMBOL = 'CP';
+const CONTRACT_NAME = 'TRUE ITALIAN PIZZA ARMY';
+const CONTRACT_SYMBOL = 'TIPA';
 const METADATA_UPDATABLE = true; // set to false if you don't want to allow metadata updates after minting
-const OWNER_ADDRESS = 'YOUR WALLET ADDRESS HERE';
-const TREASURY_ADDRESS = 'YOUR WALLET ADDRESS HERE';
+const OWNER_ADDRESS = '0xf271F17b2BffeCEE59F37a2D78664E07CBA47942';
+const TREASURY_ADDRESS = '0xf271F17b2BffeCEE59F37a2D78664E07CBA47942';
 const MAX_SUPPLY = 5000; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
-const MINT_PRICE = 1; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
+const MINT_PRICE = 0.0001; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
 const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
 const PUBLIC_MINT_START_DATE = "2022-03-20T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-02-08T11:30:48+00:00
-const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
-const ROYALTY_ADDRESS = "0xd8B808A887326F45B2D0cd999709Aa6264CeF919"; // Address that will receive the royalty
+const PRESALE_MINT_START_DATE = "2022-06-01T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const ROYALTY_SHARE = 5000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
+const ROYALTY_ADDRESS = "0xf271F17b2BffeCEE59F37a2D78664E07CBA47942"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
-const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
+const PRESALE_WHITELISTED_ADDRESSES = [""]; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
-let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
+let CONTRACT_ADDRESS = "0x1a5D009706c163bE2B0C6eb2727f9975fc1F4D8E"; // If you want to manually include it
 
 // Generic Metadata is optional if you want to reveal your NFTs
 const GENERIC = true; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
 const GENERIC_TITLE = CONTRACT_NAME; // Replace with what you want the generic titles to say if you want it to be different from the contract name.
-const GENERIC_DESCRIPTION = "REPLACE THIS"; // Replace with what you want the generic descriptions to say.
-const GENERIC_IMAGE = "https://ipfs.io/ipfs/QmUf9tDbkqnfHkQaMdFWSGAeXwVXWA61pFED7ypx4hcsfh"; // Replace with your generic image that will display for all NFTs pre-reveal.
+const GENERIC_DESCRIPTION = "Which Pizza Soldier will you get?"; // Replace with what you want the generic descriptions to say.
+const GENERIC_IMAGE = "https://ipfs.io/ipfs/bafybeifaepp5sv6os3roxw2iq76xytruccqu6o6eeifsylpkokcznortje"; // Replace with your generic image that will display for all NFTs pre-reveal.
 
 // Automatically set contract address if deployed using the deployContract.js script
 try {
